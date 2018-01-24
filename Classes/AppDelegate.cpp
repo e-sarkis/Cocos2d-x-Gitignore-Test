a22f256447aa10bb742d53b5beab855823d3435f
E Sarkis <elisha.sarkis96@gmail.com>
2018-01-24 16:57:17 -0500

    Add project (please work)

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -59,9 +59,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("Cocos2d-x-Gitignore-Test", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Install-Test", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("Cocos2d-x-Gitignore-Test");
+        glview = GLViewImpl::create("Install-Test");
 #endif
         director->setOpenGLView(glview);
     }
